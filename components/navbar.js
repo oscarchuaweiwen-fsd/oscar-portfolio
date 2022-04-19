693af2b93b9d41a177191aa27dfa429d73e1abe0
Oscar Chua Wei Wen - FSD <oscarchuaweiwen.personal@gmail.com>
2022-04-19 23:11:53 +0800

    first commit

---
+++ components/navbar.js
@@ -0,0 +1,46 @@
+import React,{useEffect,useState} from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
+function Navbar() {
+  
+  const [height,setHeight] = useState(0); 
+  useEffect(() => {
+    window.addEventListener("scroll",()=>{
+      setHeight(()=>{return window.pageYOffset})
+    })
+
+    return ()=>{
+      window.addEventListener("scroll",()=>{
+      })
+    }
+  },[])
+
+  const toggleFixedScroll = () => {
+    if(height>100){
+      return "fixed flex justify-between w-full px-5 py-5 backdrop-blur-md z-10"
+    }else{
+      return "absolute flex justify-between w-full mt-5 px-5"
+    }
+  }
+  return (
+    <div id="/">
+      <div className={toggleFixedScroll()}>
+      <Link activeClass="active" to="/" spy={true} smooth={true} offset={100} duration={2000}>
+          Oscar Chua Wei Wen
+        </Link>
+
+        <div className="flex gap-5 pr-10 cursor-pointer">
+        <Link activeClass="active" to="about" spy={true} smooth={true} offset={100} duration={2000}>
+          Test 1
+        </Link>
+          <div>Experience</div>
+          <div>Work</div>
+          <div>Contact</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
