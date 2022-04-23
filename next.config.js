1b706836217aefd68eea8223344dcd40e71ea041
Oscar Chua Wei Wen - FSD <oscarchuaweiwen.personal@gmail.com>
2022-04-23 14:14:25 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    outlook:"testingacc98@outlook.com",
-    password:"chosenone111"
-  }
+ 
 }
 
 module.exports = nextConfig
