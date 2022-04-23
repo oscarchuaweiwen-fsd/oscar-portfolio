ade1015efefcb738b8112275bb4cda7a4921d709
Oscar Chua Wei Wen - FSD <oscarchuaweiwen.personal@gmail.com>
2022-04-23 13:08:01 +0800

    Update sendMessage.js

--- pages/api/sendMessage.js
+++ pages/api/sendMessage.js
@@ -1,46 +1,48 @@
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
-      
-        let transporter = nodemailer.createTransport({
-          host: "smtpout.secureserver.net",
-          port: 465,
-          secure: true, // true for 465, false for other ports
-          auth: {
-            user: process.env.GMAIL, // generated ethereal user
-            pass: process.env.GMAIL_PASSWORD, // generated ethereal password
-          },
-        });
-        let info = await transporter.sendMail({
-            from: '"No Reply 👻" <noreply@example.com>', // sender address
-            to: "oscarchuaweiwen.personal@gmail.com,limjoanne379@gmail.com", // list of receivers
-            subject: "Oscar's portfolio responses ✔", // Subject line
-            text: "Oscar's portfolio responses", // plain text body
-            html: ` <body style="
-            display: flex; 
-            justify-content: center; 
-            align-items: center;">
-              <div>
-                Responses from Oscar's portfolio
+        
+          let transporter = nodemailer.createTransport({
+            service: "Outlook365",
+  host: "smtp.office365.com",
+  port: "587",
+  tls: {
+    ciphers: "SSLv3",
+    rejectUnauthorized: false,
+  },
+  auth: {
+    user: process.env.GMAIL,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+          });
+          let info = await transporter.sendMail({
+              from: '"No Reply 👻" <freeid1314@hotmail.com>', // sender address
+              to: "oscarchuaweiwen.personal@gmail.com,limjoanne379@gmail.com", // list of receivers
+              subject: "Oscar's portfolio responses ✔", // Subject line
+              text: "Oscar's portfolio responses", // plain text body
+              html: ` <body style="
+              display: flex; 
+              justify-content: center; 
+              align-items: center;">
                 <div>
-                  Email address: ${req.body.email}
+                  Responses from Oscar's portfolio
+                  <div>
+                    Email address: ${req.body.email}
+                  </div>
+                  <div>
+                  Contact number: ${req.body.contact}
+                 
                 </div>
                 <div>
-                Contact number: ${req.body.contact}
+                Message: ${req.body.message}
                
               </div>
-              <div>
-              Message: ${req.body.message}
-             
-            </div>
-              <div>
-            </body>`, // html body
-          });
-    
-          if(info){
-            res.status(200).json({ messageSent: true })
-          }
+                <div>
+              </body>`, // html body
+            });
       
-          console.log(process.env.GMAIL_PASSWORD,process.env.GMAIL_PASSWORD)
-          return process.env.GMAIL
+            if(info){
+              res.status(200).json({ messageSent: true })
+            }
+        
   }
